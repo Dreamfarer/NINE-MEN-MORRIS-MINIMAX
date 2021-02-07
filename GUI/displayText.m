4f60b663c486befc42e0e40117400f3ca605dd83
BE3dARt <gianluca.imbiscuso@gmail.com>
2021-02-07 17:12:01 +0100

    Commented displayText.m and made it ready to deply

--- GUI/displayText.m
+++ GUI/displayText.m
@@ -1,8 +1,14 @@
-function displayText(message, color, fontSize)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Display text beneath Nine-Men-Morris board
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%-Input-
+%message:       The actual message that gets displayed
+%fontColor:     Font color
+%fontsize:      Font Size
+function displayText(message, fontColor, fontSize)
 
 delete(findobj(gca,'Type','text','HorizontalAlignment','left'));
-test = text(0.25, -1, message,'FontUnits','normalized','FontSize',fontSize,'HorizontalAlignment','left');
-
-test(1).Color = color;
+outputText = text(0.25, -1, message,'FontUnits','normalized','FontSize',fontSize,'HorizontalAlignment','left');
+outputText(1).Color = fontColor;
 
 end
