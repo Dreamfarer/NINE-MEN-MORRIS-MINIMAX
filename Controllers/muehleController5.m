7658e38cc132bf829d132be7cb0d8e82cd4d4a33
BE3dARt <gianluca.imbiscuso@gmail.com>
2021-02-07 20:38:18 +0100

    muehleController5 commented & made ready for deployment

--- Controllers/muehleController5.m
+++ Controllers/muehleController5.m
@@ -1,13 +1,14 @@
-function b=muehleController5(b,startingPlayer)
-%Mühle controller for a human player and an improved AI, I/O via GUI
-%inputs:
-%  b  (default:empty) specifies a board (3x3x3, 0=empty; 1=mark pl1(white); -1=mark pl2(black); nan=not a valid space)
-%  startingPlayer (default:random) specifies which players (1/-1) turn it is
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Game Controller
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%-Input-
+%board:                     Nine-Men-Morris board if you wish to supply one (optional)
+%startingPlayer:            Player to start the game (optional)
+%phase(1):                  Phase of Player 1 (optional)
+%phase(2):                  Phase of Player 2 (optional)
+%stonesBeginningPhase:      How many stones there are in total left *TO LAY DOWN* in the beginning (optional)
 
-phase1=1;
-phase2=1;
-stonesBeginningPhase=18;
-removedStone = NaN;
+function muehleController5(board, startingPlayer, phase, stonesBeginningPhase)
 
 %Determine which player begins
 if ~exist('startingPlayer','var')
@@ -16,19 +17,34 @@ if ~exist('startingPlayer','var')
         startingPlayer = -1; 
     end
 end
-
 playerType = startingPlayer;
 
+%Set how many stones there are in total left *TO LAY DOWN*
+if ~exist('stonesBeginningPhase','var')
+    stonesBeginningPhase = 18;
+end
+
+%Phase of Human Player
+if ~exist('phase(1)','var')
+    phase(1) = 1;
+end
+
+%Phase of AI
+if ~exist('phase(2)','var')
+    phase(2) = 1;
+end
+
 %Create 3x3x3 board
-if ~exist('b','var') || size(b,1)~=3 || size(b,2)~=3 || size(b,3)~=3 %create board if nonexistent
+if ~exist('board','var') || size(board,1)~=3 || size(board,2)~=3 || size(board,3)~=3 %create board if nonexistent
     a = zeros(3,3,3); 
     a(2,2,:) = NaN; %NaN at every middle position since muehle has no middle position in each layer
-    b=a;
+    board=a;
 end
 
 moveTo = NaN;
 moveFrom = NaN;
-bestStoneRemove = NaN;
+bestStoneRemove = NaN; %Stone to remove (AI)
+removedStone = NaN; %If a stone was removed, this stays active for one GUI call (AI)
 
 while 1
     %Human Player
@@ -40,13 +56,14 @@ while 1
             %Count down stones
             stonesBeginningPhase=stonesBeginningPhase-1; 
             
-            %Call GUI and do the magik
-            [b, moveTo] = GUI(b, playerType, [phase1 phase2], "move", [moveFrom moveTo removedStone]);
+            %Call GUI to lay stone
+            [board, moveTo] = GUI(board, playerType, [phase(1) phase(2)], "move", [moveFrom moveTo removedStone]);
         
         %Phase 2 and 3
-        elseif phase1==2 || phase1==3 %%check for phase 2 or 3
-
-            [b, moveTo] = GUI(b, playerType, [phase1 phase2], "move", [moveFrom moveTo removedStone]);
+        elseif phase(1)==2 || phase(1)==3 %%check for phase 2 or 3
+            
+            %Call GUI to move
+            [board, moveTo] = GUI(board, playerType, [phase(1) phase(2)], "move", [moveFrom moveTo removedStone]);
             
         end
         
@@ -56,19 +73,20 @@ while 1
     else
         
         %Call GUI to tell the player that AI is calculating
-        GUI(b, playerType, [phase1 phase2], "waitForAI", [moveFrom moveTo removedStone]);
+        GUI(board, playerType, [phase(1) phase(2)], "waitForAI", [moveFrom moveTo removedStone]);
         
-        [bestScore, moveFrom, moveTo, bestStoneRemove] = minimaxMuehle2(b, 0, phase1, phase2, playerType,stonesBeginningPhase, 1);
-        if phase2==1
+        [bestScore, moveFrom, moveTo, bestStoneRemove] = minimaxMuehle2(board, 0, phase(1), phase(2), playerType,stonesBeginningPhase, 1);
+        if phase(2)==1
             stonesBeginningPhase=stonesBeginningPhase-1;
-            b(moveTo)=playerType;
+            board(moveTo)=playerType;
             moveFrom = NaN;
         else
-            if bestScore==(Inf) %AI knows that it loses -> makes the first move possible
-                possibleFrom=(find(b==playerType));
+            %AI knows that it loses -> makes the first move possible
+            if bestScore==(Inf) 
+                possibleFrom=(find(board==playerType));
                 for i=1:numel(possibleFrom)
-                    for j=1:numel(b)
-                        if isValidMove(b,possibleFrom(i),j,playerType,phase1,phase2)
+                    for j=1:numel(board)
+                        if isValidMove(board,possibleFrom(i),j,playerType,phase(1),phase(2))
                             moveFrom=possibleFrom(i);
                             moveTo=j;
                             break;
@@ -79,65 +97,67 @@ while 1
                     end
                 end
             end
-
-            b([moveFrom moveTo])=b([moveTo moveFrom]); 
+            
+            %Switch Indices
+            board([moveFrom moveTo])=board([moveTo moveFrom]); 
    
-           
         end
     end
     
     %Take away opponent's stone if you have a muehle
-    if checkMuehle(b,moveTo) 
+    if checkMuehle(board,moveTo) 
         
         %Human Player
         if playerType==1
             
-             [b, moveTo] = GUI(b, playerType, [phase1 phase2], "remove", [moveFrom moveTo removedStone]);
+            %Call GUI to remove a opponent's stone
+            [board, moveTo] = GUI(board, playerType, [phase(1) phase(2)], "remove", [moveFrom moveTo removedStone]);
             
         %AI
         else
             
             possibleRemoves=[];
-            for l=1:numel(b)
-                if validRemove(b,playerType,l) %check if there are any possible stones to remove
+            for l=1:numel(board)
+                
+                %check if there are any possible stones to remove
+                if validRemove(board,playerType,l) 
                     possibleRemoves=[possibleRemoves,l];
                 end
             end
             if ~isempty(possibleRemoves)
                 if bestScore~=(Inf) 
-                    b(bestStoneRemove)=0;
+                    board(bestStoneRemove)=0;
                     removedStone = bestStoneRemove;
                 else %AI knows that it loses -> makes the first remove possible if it makes a muehle by accident
-                    b(possibleRemoves(1))=0;
+                    board(possibleRemoves(1))=0;
                 end
             end
    
         end
         
         %Change opponent to phase 3 when they only have 3 stones remaining
-        if (playerType==1 && phase1==2) || (playerType==-1 && phase2==2)||(playerType==1 && phase1==3) || (playerType==-1 && phase2==3)
-            if sum(b==-playerType,'all')==3 
+        if (playerType==1 && phase(1)==2) || (playerType==-1 && phase(2)==2)||(playerType==1 && phase(1)==3) || (playerType==-1 && phase(2)==3)
+            if sum(board==-playerType,'all')==3 
                 if -playerType==1
-                    phase1=3;
+                    phase(1)=3;
                 else
-                    phase2=3;
+                    phase(2)=3;
                 end
             end
         end
-        
     end
     
     %Change Phase from 1 to 2 after all stones have been placed
-    if stonesBeginningPhase==0 && phase1 == 1 && phase2 == 1
-        phase1=2;
-        phase2=2;
+    if stonesBeginningPhase==0 && phase(1) == 1 && phase(2) == 1
+        phase(1)=2;
+        phase(2)=2;
         
     end
     
     %Check if game is over
-    isOver = evaluateMuehleBoard2(b, 0, phase1, phase2, -playerType, moveTo);
+    isOver = evaluateMuehleBoard2(board, 0, phase(1), phase(2), -playerType, moveTo);
     if(isOver)
-        GUI(b, playerType, [phase1 phase2], "GameOver", [moveFrom moveTo removedStone]);
+        GUI(board, playerType, [phase(1) phase(2)], "GameOver", [moveFrom moveTo removedStone]);
         break; 
     end
     playerType = -playerType;
